b14ad6290b3fede17426027db53f2b2f6bd4b560
Nicholas Pun <nicholaspun99@gmail.com>
2017-01-13 13:58:39 -0500

    defining rules for parsing prereq text

--- mainParse.py
+++ mainParse.py
@@ -10,58 +10,19 @@ courseNameLen = 6
 URL = "http://www.ucalendar.uwaterloo.ca/1617/COURSE/course-CS.html"
 
 #------------------
-# HELPER FUNCTIONS
+# MAIN FUNCTIONS
 #------------------
-def nonCoursePrereq(prereq):
-    '''
-    returns true if "students" or "Level at least" present in prereq
-    '''
-    return ('students' in prereq) or ('Level at least' in prereq)
-
-def makePrereqList(prereqStr):
-    '''
-    returns a list of parsed prerequistes 
-    '''
-    pList = prereqStr.split(';')
-    print(pList)
-    # remove non course-related prereqs
-    pList[:] = filterfalse(nonCoursePrereq, pList)
-    print(pList)
-    for x in pList:
-        x = x.split('and'))
-        
-    print(pList)
-    # Split prereqs for "One of" and "Two of" cases
-    '''
-    for p in pList:
-        p = p.upper().strip()
-        if ("ONE OF" in p):
-            p.replace("ONE OF", "")
-            p = p.split(', ')
-            p = [1, prereq]
-        elif ("TWO OF" in prereq):
-            p.replace("TWO OF", "")
-            p = p.split(', ')
-            p = [2, prereq]
-            '''
-                
-    return pList
-
-def findPrereq(courseCode):
-    '''
-    returns the prerequistes for given course code.
-    '''
-    pass 
-
 def buildDict():
     '''
     Builds a dictionary of courses from the provided URL above.
     Key -- Course Code 
     Value -- Un-modified Prerequistes
     '''
-    response = urllib.urlopen(URL) # open URL & store object
-    file = response.read().decode("utf-8") # Read & convert raw data to string
+    #response = urllib.urlopen(URL) # open URL & store object
+    #file = response.read().decode("utf-8") # Read & convert raw data to string
 
+    file = open("CSHTML.txt",'r').read()
+    
     # patterns only work for CS courses (for now) 
     pattern1 = re.compile(r'<a name = "CS(\d+)"></a>.+?<i>Prereq:(.+?)</i>') # With Course Prereqs
     pattern2 = re.compile(r'<a name = "CS(\d+)"></a>') # Course Code only
@@ -72,13 +33,104 @@ def buildDict():
     parsedOnlyCCode = re.findall(pattern2, file) # List of only course codes
 
     DictofCourses = dict(parsedWithPreReq) # Key - Course Code, Value - Prereq
-
+    
     # Set Value of "None" if Course does not have prereq
     for ccode in parsedOnlyCCode:
         if (ccode not in DictofCourses):
             DictofCourses[ccode] = "None"
 
     return DictofCourses
+
+
+def makePrereqList(prereqStr):
+    '''
+    returns a list of parsed prerequistes 
+    '''
+    pList = prereqStr.split(';')
+    
+    # remove non course-related prereqs
+    pList[:] = filterfalse(nonCoursePrereq, pList)
+
+    # split prereqs for "and" case 
+    tempList = []
+    for i in range(len(pList)):
+        tempList += pList[i].split(' and ')   
+    pList = tempList
+    
+    # Split prereqs for "One of" and "Two of" cases
+    pList = splitOneofTwoofCase(pList)
+
+          
+    return pList
+
+
+#------------------
+# HELPER FUNCTIONS
+#------------------
+def nonCoursePrereq(prereq):
+    '''
+    returns true if "students" or "Level at least" present in prereq
+    '''
+    return ('students' in prereq) or ('Level at least' in prereq)
+
+def splitOneofTwoofCase(pList):
+    '''
+    returns a list where:
+      if the prereq contains the phrase "one of", it is formatted like so:
+      "One of MATH 116, 136, 146" --> [1, ["MATH 116", "MATH 136", "MATH 146"]]
+
+      if the prereq contains the phrase "one of", it is formatted like so:
+      "Two of MATH 116, 136, 146" --> [2, ["MATH 116", "MATH 136", "MATH 146"]] 
+    '''
+    for i in range(len(pList)):
+        # This line just makes things pretty.....
+        pList[i] = pList[i].upper().strip().replace("(", "").replace(")", "")
+
+        # Below is horrible code, it splits courses so that
+        # they are individual, and inserts the department name
+        # in front of each element
+        if ("ONE OF" in pList[i]):
+            pList[i] = pList[i].replace("ONE OF ", "")
+            dept = findCourseDept(pList[i])
+            pList[i] = [1, pList[i].split(",")]
+            pList[i][1] = insertFront(dept, pList[i][1])
+        elif ("TWO OF" in pList[i]):
+            pList[i] = pList[i].replace("TWO OF", "")
+            dept = findCourseDept(pList[i])
+            pList[i] = [2, pList[i].split(",")]
+            pList[i][1] = insertFront(dept, pList[i][1])         
+    return pList
+
+def findCourseDept(course):
+    '''
+    returns the department a course belongs to.
+
+    e.g. "MATH 135" -> "MATH"
+    '''
+    dept = ""
+    courseLen = len(course)
+    for i in range(courseLen):
+        if (course[i] == " "): break
+        dept += course[i]
+    return dept
+
+def insertFront(dept, lst):
+    '''
+    returns a lst where the string dept is inserted before the start of
+    each element in lst
+    '''
+    for i in range(len(lst)):
+        if (i == 0): continue
+        lst[i] = dept + lst[i]
+    return lst
+            
+def findPrereq(courseCode):
+    '''
+    returns the prerequistes for given course code.
+    '''
+    pass 
+
+
 
 
 #------------------
@@ -88,7 +140,9 @@ def buildDict():
 courses = buildDict()
 
 # Tests:
+print("orig:", courses["330"])
 print(makePrereqList(courses["330"])) # non course-related prereq
+print("orig:", courses["349"])
 print(makePrereqList(courses["349"])) # "and" keyword 
 
 
