fdfb263bccc6eba1263656e48082c7597bf1b09e
Nicholas Pun <nicholaspun99@gmail.com>
2017-01-12 19:08:35 -0500

    trying new parse method using python regex

--- mainParse.py
+++ mainParse.py
@@ -1,14 +1,42 @@
 from html.parser import HTMLParser
 import urllib.request as urllib
 import codecs
+import re
 
+with open('miniHTML.txt', 'r') as file:
+    contents = file.read() #.split('<center><table border=0 width=80%><tr><td align=left>')
+pattern = re.compile(r'<a name = "(CS\d+)"></a>')
+parsedContents = re.findall(pattern, contents)
+
+'''
+parsedContents = []
+for line in contents:
+    parsedContents.append(re.search(pattern, line))
+'''
+print(parsedContents)
+
+
+
+'''
 courseNameLen = 6 
 URL = "http://www.ucalendar.uwaterloo.ca/1617/COURSE/course-CS.html"
 response = urllib.urlopen(URL) # open URL & store object
 file = response.read().decode("utf-8") # Read & convert raw data to string
+file.split('\n')
+print(file)
+'''
 
+# pattern only works for CS courses for now 
+'''
+pattern = re.compile(r'<a name = "CS(\d+)"></a>')
+parsedFile = re.search(pattern, file)
+print(parsedFile)
+'''
+
+'''
 course_names = []
 prereqs = []
+
 
 class MyHTMLParser(HTMLParser):
     def handle_data(self, data):
@@ -29,3 +57,4 @@ def find_prereq(course_name):
 find_prereq("CS 448")
 find_prereq("CS 135")
 find_prereq("CS 100")
+'''
