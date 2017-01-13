22a0e8c1b8f2a2c3d403939094bb79d6f10af762
Nicholas Pun <nicholaspun99@gmail.com>
2017-01-13 10:56:37 -0500

    added comments, wrapped code in functions

--- mainParse.py
+++ mainParse.py
@@ -24,6 +24,7 @@ def makePrereqList(prereqStr):
     '''
     pListTemp = prereqStr.split(';')
     pListTemp[:] = filterfalse(nonCoursePrereq, pListTemp)
+    pListTemp[:] = [x.split(" and ") for x in pListTemp]
     return pListTemp
 
 def findPrereq(courseCode):
@@ -32,25 +33,40 @@ def findPrereq(courseCode):
     '''
     pass 
 
+def buildDict():
+    '''
+    Builds a dictionary of courses from the provided URL above.
+    Key -- Course Code 
+    Value -- Un-modified Prerequistes
+    '''
+    response = urllib.urlopen(URL) # open URL & store object
+    file = response.read().decode("utf-8") # Read & convert raw data to string
+
+    # patterns only work for CS courses (for now) 
+    pattern1 = re.compile(r'<a name = "CS(\d+)"></a>.+?<i>Prereq:(.+?)</i>') # With Course Prereqs
+    pattern2 = re.compile(r'<a name = "CS(\d+)"></a>') # Course Code only
+
+    # List of (Course Code, Prereq) for courses with prereqs
+    parsedWithPreReq = re.findall(pattern1, file)
+
+    parsedOnlyCCode = re.findall(pattern2, file) # List of only course codes
+
+    DictofCourses = dict(parsedWithPreReq) # Key - Course Code, Value - Prereq
+
+    # Set Value of "None" if Course does not have prereq
+    for ccode in parsedOnlyCCode:
+        if (ccode not in DictofCourses):
+            DictofCourses[ccode] = "None"
+
+    return DictofCourses
+
+
 #------------------
 # Main Script
 #------------------
-response = urllib.urlopen(URL) # open URL & store object
-file = response.read().decode("utf-8") # Read & convert raw data to string
 
-# patterns only work for CS courses (for now) 
-pattern1 = re.compile(r'<a name = "CS(\d+)"></a>.+?<i>Prereq:(.+?)</i>') # With Course Prereqs
-pattern2 = re.compile(r'<a name = "CS(\d+)"></a>') # Course Code only
+courses = buildDict()
 
-# List of (Course Code, Prereq) for courses with prereqs
-parsedWithPreReq = re.findall(pattern1, file)
 
-parsedOnlyCCode = re.findall(pattern2, file) # List of only course codes
 
-DictofCourses = dict(parsedWithPreReq) # Key - Course Code, Value - Prereq
-
-# Set Value of "None" if Course does not have prereq
-for ccode in parsedOnlyCCode:
-    if (ccode not in DictofCourses):
-        DictofCourses[ccode] = "None"
 
