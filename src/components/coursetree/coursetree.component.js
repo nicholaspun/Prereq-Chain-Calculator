0f7fb65d0610bd03353044e934c91472c2cbd3f2
Nicholas Pun <nicholaspun99@gmail.com>
2017-04-19 09:39:56 -0400

    playing around with d3

--- src/components/coursetree/coursetree.component.js
+++ src/components/coursetree/coursetree.component.js
@@ -1,10 +1,41 @@
-import React, { Component } from 'react';
-import * as d3 from 'd3';
+// unfinished/src/components/chart.jsx
+import React       from 'react';
+impo
 
-export default class CourseTree extends Component {
-  render () {
+var Chart = React.createClass({
+  propTypes: {
+    data: React.PropTypes.array,
+    domain: React.PropTypes.object
+  },
+
+  componentDidMount: function() {
+    var el = this.getDOMNode();
+    d3Chart.create(el, {
+      width: '100%',
+      height: '300px'
+    }, this.getChartState());
+  },
+
+  componentDidUpdate: function() {
+    var el = this.getDOMNode();
+    d3Chart.update(el, this.getChartState());
+  },
+
+  getChartState: function() {
+    return {
+      data: this.props.data,
+      domain: this.props.domain
+    };
+  },
+
+  componentWillUnmount: function() {
+    var el = this.getDOMNode();
+    d3Chart.destroy(el);
+  },
+
+  render: function() {
     return (
-      
-    )
+      <div className="Chart"></div>
+    );
   }
-}
+});
